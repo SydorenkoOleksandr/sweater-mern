0e782072bc141e43a4b36e4c930a7db6891a7ee0
unknown <semidenisjj@gmail.com>
2020-06-18 17:54:20 +0300

    add build client to node executor

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ app.use(express.json({ extended:true }))
 app.use('/api/auth', require('./routes/auth.routes'));    // router authorization
 app.use('/api/card', require('./routes/card.routes'));   // router to DB cards
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      })
+    }
 const PORT = config.get("port") || 5000  // port for localhost from config
 
 async function start() {
